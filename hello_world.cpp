65df80b3f2079e102d61662eff8460200c12a6c7
Soham K <sohamlab05@gmail.com>
2025-02-07 14:40:13 +0530

    Added Comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
+//In this section we include the libraries that we use in our program, These are called "Header files" 
 #include <iostream>
 using namespace std;
 
+//In this Program we print "Hello World" as output, Here we Use "cout" to print our output.
+
+//main() function is the function which executes on running our Program, It is necessary in every C++ program. 
 int main(){
 
     cout<<"Hello World";
